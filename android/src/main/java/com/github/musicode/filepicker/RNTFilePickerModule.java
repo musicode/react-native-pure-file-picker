64df1fd0689711a5c08e0bec7942cdf142fcd509
musicode <musicode@qq.com>
2019-06-18 17:45:31 +0800

    update

--- android/src/main/java/com/github/musicode/filepicker/RNTFilePickerModule.java
+++ android/src/main/java/com/github/musicode/filepicker/RNTFilePickerModule.java
@@ -14,8 +14,6 @@ import com.github.herokotlin.filepicker.FilePickerCallback;
 import com.github.herokotlin.filepicker.FilePickerConfiguration;
 import com.github.herokotlin.filepicker.model.PickedFile;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 
 public class RNTFilePickerModule extends ReactContextBaseJavaModule {
@@ -61,36 +59,36 @@ public class RNTFilePickerModule extends ReactContextBaseJavaModule {
         FilePickerCallback callback = new FilePickerCallback() {
 
             @Override
-            public void onCancel(@NotNull Activity activity) {
+            public void onCancel(Activity activity) {
                 activity.finish();
                 promise.reject("-1", "cancel");
             }
 
             @Override
-            public void onPermissionsNotGranted(@NotNull Activity activity) {
+            public void onPermissionsNotGranted(Activity activity) {
                 activity.finish();
                 promise.reject("1", "has no permissions");
             }
 
             @Override
-            public void onPermissionsDenied(@NotNull Activity activity) {
+            public void onPermissionsDenied(Activity activity) {
                 activity.finish();
                 promise.reject("2", "you denied the requested permissions.");
             }
 
             @Override
-            public void onExternalStorageNotWritable(@NotNull Activity activity) {
+            public void onExternalStorageNotWritable(Activity activity) {
                 activity.finish();
                 promise.reject("3", "external storage is not writable");
             }
 
             @Override
-            public void onPermissionsGranted(@NotNull Activity activity) {
+            public void onPermissionsGranted(Activity activity) {
 
             }
 
             @Override
-            public void onSubmit(@NotNull Activity activity, List<PickedFile> list) {
+            public void onSubmit(Activity activity, List<PickedFile> list) {
 
                 activity.finish();
 
