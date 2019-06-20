f52081ba83856fc89906e73a3f5554f9fa770be0
musicode <musicode@qq.com>
2019-06-20 17:03:30 +0800

    update

---
+++ android/src/main/java/com/github/musicode/filepicker/RNTFilePickerModule.kt
@@ -0,0 +1,120 @@
+package com.github.musicode.filepicker
+
+import android.app.Activity
+import com.facebook.react.ReactActivity
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.modules.core.PermissionAwareActivity
+import com.github.herokotlin.filepicker.FilePickerActivity
+import com.github.herokotlin.filepicker.FilePickerCallback
+import com.github.herokotlin.filepicker.FilePickerConfiguration
+import com.github.herokotlin.filepicker.model.PickedFile
+
+class RNTFilePickerModule1(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "RNTFilePicker"
+    }
+
+    private var permissionListener = { requestCode: Int, permissions: Array<out String>?, grantResults: IntArray? ->
+        if (permissions != null && grantResults != null) {
+            FilePickerActivity.permission.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        true
+    }
+
+    @ReactMethod
+    fun open(options: ReadableMap, promise: Promise) {
+
+        val permission = FilePickerActivity.permission
+
+        permission.onExternalStorageNotWritable = {
+            promise.reject("3", "external storage is not writable")
+        }
+
+        permission.onPermissionsDenied = {
+            promise.reject("2", "you denied the requested permissions.")
+        }
+
+        permission.onPermissionsNotGranted = {
+            promise.reject("1", "has no permissions")
+        }
+
+        permission.onRequestPermissions = { activity, list, requestCode ->
+            if (activity is ReactActivity) {
+                activity.requestPermissions(list, requestCode, permissionListener)
+            }
+            else if (activity is PermissionAwareActivity) {
+                (activity as PermissionAwareActivity).requestPermissions(list, requestCode, permissionListener)
+            }
+        }
+
+        if (permission.checkExternalStorageWritable()) {
+            permission.requestPermissions(currentActivity!!) {
+                openActivity(options, promise)
+            }
+        }
+
+    }
+
+    private fun openActivity(options: ReadableMap, promise: Promise) {
+
+        val configuration = object : FilePickerConfiguration() {
+
+        }
+
+        configuration.maxSelectCount = 1
+
+        if (options.hasKey("cancelButtonTitle")) {
+            configuration.cancelButtonTitle = options.getString("cancelButtonTitle")!!
+        }
+        if (options.hasKey("submitButtonTitle")) {
+            configuration.submitButtonTitle = options.getString("submitButtonTitle")!!
+        }
+
+        if (options.hasKey("dateFormatCurrentDate")) {
+            configuration.dateFormatCurrentDate = options.getString("dateFormatCurrentDate")!!
+        }
+        if (options.hasKey("dateFormatCurrentYear")) {
+            configuration.dateFormatCurrentYear = options.getString("dateFormatCurrentYear")!!
+        }
+        if (options.hasKey("dateFormatAnyTime")) {
+            configuration.dateFormatAnyTime = options.getString("dateFormatAnyTime")!!
+        }
+
+        val callback = object : FilePickerCallback {
+
+            override fun onCancel(activity: Activity) {
+                activity.finish()
+                promise.reject("-1", "cancel")
+            }
+
+            override fun onSubmit(activity: Activity, list: List<PickedFile>) {
+
+                activity.finish()
+
+                val map = Arguments.createMap()
+                val (path, name, size) = list[0]
+
+                map.putString("path", path)
+                map.putString("name", name)
+                map.putInt("size", size)
+
+                promise.resolve(map)
+
+            }
+        }
+
+        FilePickerActivity.configuration = configuration
+        FilePickerActivity.callback = callback
+
+        FilePickerActivity.newInstance(currentActivity!!)
+
+    }
+
+}
